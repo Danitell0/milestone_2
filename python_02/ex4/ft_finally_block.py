fa97036fecc0724c697039c27a55ec0b1f62ac97
Danitell0 <danielfernandesdesigns@gmail.com>
2026-05-13 21:17:20 +0200

    Python_02 {ex4}

--- python_02/ex4/ft_finally_block.py
+++ python_02/ex4/ft_finally_block.py
@@ -15,17 +15,30 @@ class WaterError(GardenError):
     def __init__(self, message: str = "Unknown plant error"):
         super().__init__(message)
 
+
 def water_plant(plant_name: str) -> None:
+    if plant_name == plant_name.capitalize():
+        print(f"Watering {plant_name}: [OK]")
+    else:
+        raise PlantError(f"Invalid plant name to water: {plant_name}")
 
 
-def test_watering_system() -> None:
+def test_watering_system(plant_list: list[str]) -> None:
     print("Opening watering system")
     try:
-
+        for i in plant_list:
+            water_plant(i)
     except PlantError as e:
         print(f"Caught PlantError: {e}")
+        print(".. ending tests and returning to main")
     finally:
-        print("Closing watering system")
+        print("Closing watering system\n")
+
 
 if __name__ == "__main__":
-    print("=== Garden Watering System ===")
+    print("=== Garden Watering System ===\n")
+    print("Testing valid plants...")
+    test_watering_system(["Tomato", "Lettuce", "Carrots"])
+    print("Testing invalid plants...")
+    test_watering_system(["Tomato", "lettuce", "carrots"])
+    print("Cleanup always happens, even with errors!")
