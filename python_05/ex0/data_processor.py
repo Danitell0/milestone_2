7b9ce87af6c9290f768a474de1e65740ce039b03
Daniel Filipe Moreira Fernandes <danmorei@f1r4s18.codam.nl>
2026-05-22 18:59:04 +0200

    update

--- python_05/ex0/data_processor.py
+++ python_05/ex0/data_processor.py
@@ -4,24 +4,34 @@ from abc import ABC, abstractmethod
 from typing import Any
 
 class DataProcessor(ABC):
+    def __init__(self) -> None:
+        self.ingest_data: list[Any] = []
+        self.counter = 0
+
     @abstractmethod
     def validate(self, data: Any) -> bool:
-        pass
+        ...
     @abstractmethod
     def ingest(self, data: Any) -> None:
-        pass
+        ...
     def output(self) -> tuple[int, str]:
-        
+        extracted = self.ingest_data.pop(0)
+        return (self.counter, str(extracted))
+
 
 class NumericProcessor(DataProcessor):
-    def __init__(self) -> None:
-        self.ingest_data = []
-
     def ingest(self, data: int | float | list[int | float]) -> None:
-        self.ingest_data.append(data)
+        if isinstance(data, list):
+            for content in data:
+                self.ingest_data.append(str(content))
+                self.counter += 1
+        else:
+            self.ingest_data.append(str(data))
+            self.counter += 1
 
-    def validate(self, data) -> bool:
-        
+    def validate(self, data: Any) -> bool:
+        return isinstance(data, (int, float)) or all(isinstance(content, (int, float)) for content in data)
+
 
 class TextProcessor(DataProcessor):
     def __init__(self):
@@ -30,7 +40,8 @@ class TextProcessor(DataProcessor):
     def ingest(self, data: str | list[str]) -> None:
         self.ingest_data.append(data)
 
-    def validate(self, data) -> bool:
+    def validate(self, data: Any) -> bool:
+        return isinstance(data, str) or all(isinstance(content, (str)) for content in data)
 
 
 class LogProcessor(DataProcessor):
@@ -40,11 +51,15 @@ class LogProcessor(DataProcessor):
     def ingest(self, data: dict) -> None:
         self.ingest_data.append(data)
 
-    def validate(self, data) -> bool:
+    def validate(self, data: Any) -> bool:
+        return isinstance(data, dict) or all(isinstance(content, (dict)) for content in data)
         
 
 def main() -> None:
     print("=== Code Nexus - Data Processor ===\n")
+
+    print("Testing Numeric Processor...")
+    
 
 
 if __name__ == "__main__":
