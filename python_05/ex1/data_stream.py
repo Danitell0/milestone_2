addc72df083dcd7847f41314ea92d5b3b1fd74bb
Danitell0 <danielfernandesdesigns@gmail.com>
2026-05-24 17:26:44 +0200

    Python_05 {ex0}

--- python_05/ex1/data_stream.py
+++ python_05/ex1/data_stream.py
@@ -1,25 +1,40 @@
 #!/usr/bin/env python3
 
-from abc import ABC, abstractmethod
-from typing import Any
+import abc
+import typing
 
 
-class DataProcessor(ABC):
+class DataProcessor(abc.ABC):
     def __init__(self) -> None:
-        self.ingest_data: list[Any] = []
+        self.ingest_data: list[typing.Any] = []
         self.counter = 0
 
-    @abstractmethod
-    def validate(self, data: Any) -> bool:
+    @abc.abstractmethod
+    def validate(self, data: typing.Any) -> bool:
         ...
 
-    @abstractmethod
-    def ingest(self, data: Any) -> None:
+    @abc.abstractmethod
+    def ingest(self, data: typing.Any) -> None:
         ...
 
     def output(self) -> tuple[int, str]:
         extracted = self.ingest_data.pop(0)
         return extracted
+
+
+class DataStream():
+    def __init__(self, stream: list[typing.Any]) -> None:
+        ...
+
+    def register_processor(self, proc: DataProcessor) -> None:
+        ...
+
+    def process_stream(self, stream: list[typing.Any]) -> None:
+        ...
+
+    def print_processors_stats(self) -> None:
+        print("== DataStream statistics ==")
+
 
 
 class NumericProcessor(DataProcessor):
@@ -34,7 +49,7 @@ class NumericProcessor(DataProcessor):
             self.ingest_data.append((self.counter, str(data)))
             self.counter += 1
 
-    def validate(self, data: Any) -> bool:
+    def validate(self, data: typing.Any) -> bool:
         if isinstance(data, list):
             return all(isinstance(content, (int, float)) for content in data)
         else:
@@ -53,7 +68,7 @@ class TextProcessor(DataProcessor):
             self.ingest_data.append((self.counter, data))
             self.counter += 1
 
-    def validate(self, data: Any) -> bool:
+    def validate(self, data: typing.Any) -> bool:
         if isinstance(data, list):
             return all(isinstance(content, str) for content in data)
         else:
@@ -76,7 +91,7 @@ class LogProcessor(DataProcessor):
                 f"{data['log_level']}: {data['log_message']}"))
             self.counter += 1
 
-    def validate(self, data: Any) -> bool:
+    def validate(self, data: typing.Any) -> bool:
         if isinstance(data, list):
             return all(
                 all(isinstance(key, str) and isinstance(value, str)
@@ -89,53 +104,8 @@ class LogProcessor(DataProcessor):
             return False
 
 
-def main() -> None:
-    print("=== Code Nexus - Data Processor ===\n")
-
-    print("Testing Numeric Processor...")
-    np = NumericProcessor()
-    print(f"Trying to validate input '42': {np.validate(42)}")
-    print(f"Trying to validate input 'Hello': {np.validate('Hello')}")
-    print("Test invalid ingestion of string 'foo' without prior validation:")
-    try:
-        np.ingest('foo')
-    except Exception as e:
-        print(f"Got exception: {e}")
-    np_list = [1, 2, 3, 4, 5]
-    print(f"Processing data: {np_list}")
-    np.ingest(np_list)
-    print("Extracting 3 values...")
-    for i in range(3):
-        popped = np.output()
-        print(f"Numeric value {popped[0]}: {popped[1]}")
-
-    print("\nTesting Text Processor...")
-    tp = TextProcessor()
-    print(f"Trying to validate input '42': {tp.validate(42)}")
-    tp_list = ['Hello', 'Nexus', 'World']
-    print(f"Processing data: {tp_list}")
-    try:
-        tp.ingest(tp_list)
-    except Exception as e:
-        print(f"Got exception: {e}")
-    print("Extracting 1 value...")
-    popped = tp.output()
-    print(f"Text value {popped[0]}: {popped[1]}")
-
-    print("\nTesting Log Processor...")
-    lp = LogProcessor()
-    print(f"Trying to validate input 'Hello': {lp.validate('Hello')}")
-    lp_list = [{'log_level': 'NOTICE', 'log_message': 'Connection to server'},
-               {'log_level': 'ERROR', 'log_message': 'Unauthorized access!!'}]
-    print(f"Processing data: {lp_list}")
-    try:
-        lp.ingest(lp_list)
-    except Exception as e:
-        print(f"Got exception: {e}")
-    print("Extracting 2 values...")
-    for i in range(2):
-        popped = lp.output()
-        print(f"Log entry {popped[0]}: {popped[1]}")
+def main():
+    print("=== Code Nexus - Data Stream ===\n")
 
 
 if __name__ == "__main__":
