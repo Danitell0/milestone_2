f86594c29a7bc18de6f338e610050af0c9647d0e
Daniel Filipe Moreira Fernandes <danmorei@f1r4s18.codam.nl>
2026-06-09 19:14:07 +0200

    Python_07 {Done}

--- python_07/ex2/strategies.py
+++ python_07/ex2/strategies.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import abc
+import typing
 from ex0.creatures import Creature
+from ex1.capabilities import TransformCapability, HealCapability
 
 
 class BattleStrategy(abc.ABC):
@@ -18,11 +20,32 @@ class NormalStrategy(BattleStrategy):
     def act(self, creature: Creature) -> None:
         print(creature.attack())
 
-    def is_valid(self, creature: Creature) -> bool:
+    def is_valid(self, _: Creature) -> bool:
         return True
 
+
 class AggressiveStrategy(BattleStrategy):
-    ...
+    def act(self, creature: Creature) -> None:
+        if not self.is_valid(creature):
+            raise Exception(f"Invalid Creature '{creature.name}'"
+                            f" for this aggressive strategy")
+        t_creature = typing.cast(TransformCapability, creature)
+        print(t_creature.transform())
+        print(creature.attack())
+        print(t_creature.revert())
+
+    def is_valid(self, creature: Creature) -> bool:
+        return isinstance(creature, TransformCapability)
+
 
 class DefensiveStrategy(BattleStrategy):
-    ...
+    def act(self, creature: Creature) -> None:
+        if not self.is_valid(creature):
+            raise Exception(f"Invalid Creature '{creature.name}'"
+                            f" for this defensive strategy")
+        h_creature = typing.cast(HealCapability, creature)
+        print(creature.attack())
+        print(h_creature.heal())
+
+    def is_valid(self, creature: Creature) -> bool:
+        return isinstance(creature, HealCapability)
--- python_07/tournament.py
+++ python_07/tournament.py
@@ -1,24 +1,53 @@
 #!/usr/bin/env python3
 
-from ex2.strategies import BattleStrategy
+from ex0.creatures import CreatureFactory
+from ex0.factories import FlameFactory, AquaFactory
 from ex1.factories import HealingCreatureFactory, TransformCreatureFactory
-from ex0 import FlameFactory, CreatureFactory
+from ex2.strategies import (BattleStrategy,
+                            NormalStrategy,
+                            AggressiveStrategy,
+                            DefensiveStrategy)
 
 
-def battle() -> None:
-    ...
+def battle(opponents: list[tuple[CreatureFactory, BattleStrategy]]) -> None:
+    print("*** Tournament ***")
+    print(f"{len(opponents)} opponents involved")
+
+    for i in range(len(opponents)):
+        for j in range(i + 1, len(opponents)):
+            factory_i, strategy_i = opponents[i]
+            factory_j, strategy_j = opponents[j]
+            creature_i = factory_i.create_base()
+            creature_j = factory_j.create_base()
+
+            print("\n* Battle *")
+            print(creature_i.describe())
+            print(" vs")
+            print(creature_j.describe())
+            print(" now fight!")
+            try:
+                strategy_i.act(creature_i)
+                strategy_j.act(creature_j)
+            except Exception as e:
+                print(f"Battle error, aborting tournament: {e}")
 
 
 def main() -> None:
-    heal_factory = HealingCreatureFactory()
-    trans_factory = TransformCreatureFactory()
-    flame_factory = FlameFactory()
-
-
     print("Tournament 0 (basic)")
-
     print(" [ (Charmander+Normal), (Healing+Defensive) ]")
-    
+    battle([(FlameFactory(), NormalStrategy()),
+            (HealingCreatureFactory(), DefensiveStrategy())])
+
+    print("\nTournament 1 (error)")
+    print(" [ (Charmander+Aggressive), (Healing+Defensive) ]")
+    battle([(FlameFactory(), AggressiveStrategy()),
+            (HealingCreatureFactory(), DefensiveStrategy())])
+
+    print("\nTournament 2 (multiple)")
+    print(" [ (Squirtle+Normal), (Healing+Defensive), (Transform+Agressive) ]")
+    battle([(AquaFactory(), NormalStrategy()),
+            (HealingCreatureFactory(), DefensiveStrategy()),
+            (TransformCreatureFactory(), AggressiveStrategy())])
 
 
 if __name__ == "__main__":
