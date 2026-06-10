cf693eeea640cd458d5873b3897b0d019fed7d6c
Daniel Filipe Moreira Fernandes <danmorei@f1r4s18.codam.nl>
2026-06-10 14:57:35 +0200

    update

--- python_08/ex0/construct.py
+++ python_08/ex0/construct.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 
 
 def main() -> None:
-    print
+    print("\nMATRIX STATUS: You're still plugged in\n")
 
+    print(f"Current Python: {sys.executable}")
+    print("Virtual Enviroment: ", end="")
+    if not os.environ.get('VIRTUAL_ENV'):
+        print("None detected")
+    else:
+        print(os.environ['PATH'])
+    print()
 
 if __name__ == "__main__":
     main()
