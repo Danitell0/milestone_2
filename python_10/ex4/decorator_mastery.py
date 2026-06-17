4530ad54b05d3699222c994e6de7506315608f42
Danitell0 <danielfernandesdesigns@gmail.com>
2026-06-17 23:39:55 +0200

    update

---
+++ python_10/ex4/decorator_mastery.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+def spell_timer(func: Callable) -> Callable:
+    ...
+
+
+def power_validator(min_power: int) -> Callable:
+    ...
+
+
+def retry_spell(max_attempts: int) -> Callable:
+    ...
+
+
+class MageGuild:
+    @staticmethod
+    def validate_mage_name(name: str) -> bool:
+        ...
+
+    def cast_spell(self, spell_name: str, power: int) -> str:
+        ...
+
+
+def main() -> None:
+
+
+if __name__ == "__main__":
+    main()
