a52882ff3ecaeb0b95b1506bf3ca704481b6a738
Daniel Filipe Moreira Fernandes <danmorei@f1r4s18.codam.nl>
2026-06-18 17:20:25 +0200

    update

--- python_10/ex4/decorator_mastery.py
+++ python_10/ex4/decorator_mastery.py
@@ -1,15 +1,58 @@
 #!/usr/bin/env python3
 
+from collections.abc import Callable
+from functools import wraps
+from typing import Any
+import time
+import random
+
+
 def spell_timer(func: Callable) -> Callable:
-    ...
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        print(f"Casting {func.__name__}...")
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"Spell completed in {(end - start):.3f} seconds")
+        return result
+    return wrapper
+
+
+@spell_timer
+def helper_spell(spell: str) -> str:
+    # time.sleep(3)
+    return f"Casting {spell}!!"
 
 
 def power_validator(min_power: int) -> Callable:
-    ...
+    def decorator(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args, **kwargs) -> str:
+            if args[0] >= min_power:
+                return func(*args, **kwargs)
+            else:
+                return "Insufficient power for this spell"
+        return wrapper
+    return decorator
+
+@power_validator(50)
+def helper_xp(power: int, target: str) -> str:
+    return f"{target} gained {power}XP!"
 
 
 def retry_spell(max_attempts: int) -> Callable:
-    ...
+    def decorator(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args, **kwargs) -> Any:
+            for attempt in range(1, max_attempts + 1):
+                try:
+                    return func(*args, **kwargs)
+                except Exception:
+                    print(f"Spell failed, retrying... (attempt {attempt}/{max_attempts})")
+            return f"Spell casting failed after {max_attempts} attempts"
+        return wrapper
+    return decorator
 
 
 class MageGuild:
@@ -22,6 +65,7 @@ class MageGuild:
 
 
 def main() -> None:
+    
 
 
 if __name__ == "__main__":
